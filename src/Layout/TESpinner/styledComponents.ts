085154f5989f0b5466321058af0a6014cbe52a2a
Stuart Casarotto <stuart.casarotto@38thstreetstudios.com>
2019-11-11 07:52:13 -0500

    fixed color rings

--- src/Layout/TESpinner/styledComponents.ts
+++ src/Layout/TESpinner/styledComponents.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import { colorBetweenColors } from 'helpers'
+import { colorBetweenColors } from './../../helpers'
 
 interface calculateRingStyles {
 	totalRings: number
