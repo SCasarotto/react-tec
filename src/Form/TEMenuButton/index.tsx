89275bc19414ee6c6d74bd87add20c41210e6a57
Stuart Casarotto <stuart.casarotto@38thstreetstudios.com>
2019-11-27 09:21:30 -0500

    TEMenuButton v1

--- src/Form/TEMenuButton/index.tsx
+++ src/Form/TEMenuButton/index.tsx
@@ -1,16 +1,68 @@
-import React from 'react'
+import React, { useState, useRef, useCallback, useEffect, ReactNode } from 'react'
+import { Reference, Popper, PopperProps, PopperChildrenProps } from 'react-popper'
 
-import { Button } from './styledComponents'
+import { TEButton } from './../TEButton'
 
-export interface TEMenuButtonProps
-	extends React.DetailedHTMLProps<
-		React.ButtonHTMLAttributes<HTMLButtonElement>,
-		HTMLButtonElement
-	> {
-	//TODO: Fix this. styled-components is weird with refs
-	ref?: any
+export interface TEMenuButtonChildrenProps extends PopperChildrenProps {
+	hideMenu(): void
+}
+export interface TEMenuButtonProps extends Omit<PopperProps, 'children'> {
+	className?: string
+	title: ReactNode
+	children(props: TEMenuButtonChildrenProps): ReactNode
 }
 export const TEMenuButton: React.FC<TEMenuButtonProps> = (props) => {
-	const { className = '', type = 'button', ...rest } = props
-	return <Button className={`TEButton ${className}`} type={type} {...rest} />
+	const { placement, className = '', title, children, ...rest } = props
+	const [menuVisible, setMenuVisible] = useState(false)
+	const containerRef = useRef(null)
+	const hideMenu = useCallback(() => setMenuVisible(false), [])
+	const handleOutsideClick = useCallback((e) => {
+		// @ts-ignore: Object is possibly 'null'.
+		if (containerRef && containerRef.current && containerRef.current.contains(e.target)) {
+			return
+		}
+		setMenuVisible(false)
+		return
+	}, [])
+	useEffect(() => {
+		if ('ontouchend' in window) {
+			document.addEventListener('touchend', handleOutsideClick)
+		} else {
+			document.addEventListener('click', handleOutsideClick)
+		}
+		return () => {
+			document.removeEventListener('touchend', handleOutsideClick)
+			document.removeEventListener('click', handleOutsideClick)
+		}
+	}, [handleOutsideClick])
+
+	return (
+		<div className={`TEMenuButton ${className}`} ref={containerRef}>
+			<Reference>
+				{({ ref }) => (
+					<TEButton
+						className='TEMenuButtonButton'
+						ref={ref}
+						onClick={() => setMenuVisible(!menuVisible)}
+					>
+						{title}
+					</TEButton>
+				)}
+			</Reference>
+			<Popper placement={placement} {...rest}>
+				{(popperData) =>
+					menuVisible ? (
+						<div
+							className='TEMenuButtonMenu'
+							ref={popperData.ref}
+							style={popperData.style}
+							data-placement={popperData.placement}
+						>
+							{children({ ...popperData, hideMenu })}
+						</div>
+					) : null
+				}
+			</Popper>
+		</div>
+	)
 }
